c445a5cc60ae2ac88c756cd775affef26757bde4
Chocolate1999 <2816778271@qq.com>
2020-10-13 20:22:53 +0800

    头部按钮区域

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 
 import {
-	HeaderWrapper,
-	Logo,
-	// Nav,
-	// NavItem,
-	// SearchWrapper,
-	// NavSearch,
-	// SearchInfo,
-	// SearchInfoTitle,
-	// SearchInfoSwitch,
-	// SearchInfoList,
-	// SearchInfoItem,
-	// Addition,
-	// Button
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  SearchWrapper,
+  NavSearch,
+  // SearchInfo,
+  // SearchInfoTitle,
+  // SearchInfoSwitch,
+  // SearchInfoList,
+  // SearchInfoItem,
+  Addition,
+  Button
 } from './style';
 
 class Header extends Component {
   render() {
     return (
       <HeaderWrapper>
-        <Logo/>
+        <Logo />
+        <Nav>
+          <NavItem className='left active'>首页</NavItem>
+          <NavItem className='left'>下载App</NavItem>
+          <NavItem className='right'>登录</NavItem>
+          <NavItem className='right'>Aa</NavItem>
+          <SearchWrapper>
+            <NavSearch></NavSearch>
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+            <Button className='writting'>
+							写文章
+						</Button>
+          <Button className='reg'>注册</Button>
+        </Addition>
       </HeaderWrapper>
     )
   }
