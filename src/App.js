f7d71b5426e30791ed1c4d18550508d0fe68a515
Chocolate1999 <2816778271@qq.com>
2020-10-17 09:37:52 +0800

    配置路由

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Header from './common/header';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { GlobalStyle } from './style.js';
 import { GlobalIcon } from './statics/iconfont/iconfont';
 import store from './store';
