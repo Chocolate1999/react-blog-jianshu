4dbbf23fc26b1d1eedfc8eb9a5274e424eeb53cb
Chocolate1999 <2816778271@qq.com>
2020-10-18 23:20:42 +0800

    home页面开发

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Header from './common/header';
+import Home from './pages/home';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { GlobalStyle } from './style.js';
 import { GlobalIcon } from './statics/iconfont/iconfont';
@@ -10,11 +11,14 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div>
-          <Header />
-          <GlobalStyle />
-          <GlobalIcon />
-        </div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path='/' exact component={Home}></Route>
+            <GlobalStyle />
+            <GlobalIcon />
+          </div>
+        </BrowserRouter>
       </Provider >
     );
   }
